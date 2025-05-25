2ef9780dbb5cfa0b3d2a6565faf569474cb8ebc7
gleicekelly13 <gleice.kelly22@hotmail.com>
2025-05-25 14:47:21 -0300

    Adição de estados e inputs controlados ao ConversorMoeda

--- src/components/ConversorMoeda.jsx
+++ src/components/ConversorMoeda.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 function ConversorMoeda() {
+    const [valor, setValor] = useState('');
+    const [moedaOrigem, setMoedaOrigem] = useState('USD');
+    const [moedaDestino, setMoedaDestino] = useState ('BRL');
+    const [resultado, setResultado] = useState(null);
+
     return (
         <div>
             <h1>Conversor de Moedas</h1>
 
             <div>
-                <input type="number" placeholder="Valor" />
+                <input 
+                    type="number"
+                    value={valor}  /* Mostra o que está no estado `valor`. */
+                    onChange={(e) => setValor(e.target.value)} /* Atualiza o estado `valor` quando o usuário digita. */
+                    placeholder="Valor" 
+                />
 
-                <select>
+                {/* Moeda que o usuário escolheu como origem */}
+                <select value={moedaOrigem} onChange={(e) => setMoedaOrigem(e.target.value)}>
                     <option value="USD">Dólar (USD)</option>
                     <option value="BRL">Real (BRL)</option>
                     <option value="EUR">Euro (EUR)</option>
@@ -14,7 +27,8 @@ function ConversorMoeda() {
                     <option value="ARS">Peso argentino (ARS)</option>
                 </select>
 
-                <select>
+                {/* Moeda de destino */}
+                <select value={moedaDestino} onChange={(e) => setMoedaDestino(e.target.value)}>
                     <option value="USD">Dólar (USD)</option>
                     <option value="BRL">Real (BRL)</option>
                     <option value="EUR">Euro (EUR)</option>
@@ -25,7 +39,8 @@ function ConversorMoeda() {
                 <button>Converter</button>
             </div>
 
-            <p>Resultado...</p>
+            {/* Mostrar o valor convertido */}
+            <p>{resultado ? `Resultado: ${resultado}` : 'Resultado...'}</p>
         </div>
     )
 }

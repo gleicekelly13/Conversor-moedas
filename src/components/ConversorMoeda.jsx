8df143eb59566d80574def51c9d0349308b62cda
gleicekelly13 <gleice.kelly22@hotmail.com>
2025-07-01 10:36:30 -0300

    Estilização do modo dark no título

--- src/components/ConversorMoeda.jsx
+++ src/components/ConversorMoeda.jsx
@@ -67,7 +67,7 @@ function ConversorMoeda() {
                     </button>
 
                     <div className="flex flex-col gap-5 bg-white dark:bg-gray-900 p-8 rounded-lg shadow-lg w-full max-w-md">
-                        <h1 className="text-center text-2xl font-medium text-emerald-950">Conversor de Moedas</h1>
+                        <h1 className="text-center text-2xl font-medium text-emerald-950 dark:text-emerald-400">Conversor de Moedas</h1>
 
                         <div className="flex flex-col gap-4 mt-6">
                             <input 
